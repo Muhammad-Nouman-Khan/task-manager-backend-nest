726b7bbcb176440c0f1a217d43f55992c994d67f
Muhammad-Nouman-Khan <nouman.younas.nyk@gmail.com>
2025-08-12 16:20:23 +0500

    updates

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -12,7 +12,6 @@ import {
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
- 
 
 @Controller('users')
 @UseInterceptors(ClassSerializerInterceptor)
